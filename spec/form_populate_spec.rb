3da354bd171ea50a4d4569cf9b0a3fb7f3d0e5a9
Alexei Barantsev <barancev@gmail.com>
2011-02-18 03:34:25 +0300

    Tests simplified slightly

--- spec/form_populate_spec.rb
+++ spec/form_populate_spec.rb
@@ -7,37 +7,36 @@ describe "Form" do
   include Aux
 
   it "should populate textbox" do
-    populate_and_check 'http://www.htmlcodetutorial.com/forms/index_famsupp_1.html', 1, {'color' => 'red'}
+    populate_and_check 'http://www.htmlcodetutorial.com/forms/index_famsupp_1.html', {'color' => 'red'}
   end
 
   it "should populate multiple textboxes" do
-    populate_and_check 'http://www.htmlcodetutorial.com/forms/_INPUT_TYPE_PASSWORD.html', 1, {'realname' => 'John Smith', 'mypassword' => 'qwerty'}
+    populate_and_check 'http://www.htmlcodetutorial.com/forms/_INPUT_TYPE_PASSWORD.html', {'realname' => 'John Smith', 'mypassword' => 'qwerty'}
   end
 
   it "should populate checkboxes" do
-    populate_and_check 'http://www.htmlcodetutorial.com/forms/_INPUT_TYPE_CHECKBOX.html', 2, {'mushrooms' => true, 'greenpeppers' => false, 'olives' => true, 'onions' => false}
+    populate_and_check 'http://www.htmlcodetutorial.com/forms/_INPUT_TYPE_CHECKBOX.html', {'mushrooms' => true, 'greenpeppers' => false, 'olives' => true, 'onions' => false}, 1
   end
 
   it "should populate textarea" do
-    populate_and_check 'http://www.htmlcodetutorial.com/forms/_TEXTAREA.html', 1, { 'comments' => 'bla bla bla'}
+    populate_and_check 'http://www.htmlcodetutorial.com/forms/_TEXTAREA.html',{ 'comments' => 'bla bla bla'}
   end
 
   it "should populate select" do
-    populate_and_check 'http://www.htmlcodetutorial.com/forms/_SELECT_onFocus.html', 1, { 'size' => 'm', 'phone' => '+0 123 4567890' }
+    populate_and_check 'http://www.htmlcodetutorial.com/forms/_SELECT_onFocus.html', { 'size' => 'm', 'phone' => '+0 123 4567890' }
   end
 
   it "should populate multiple select" do
-    populate_and_check 'http://www.htmlcodetutorial.com/forms/_SELECT_MULTIPLE.html', 1, { 'toppings' => ['greenpeppers', 'tomatoes'] }
+    populate_and_check 'http://www.htmlcodetutorial.com/forms/_SELECT_MULTIPLE.html', { 'toppings' => ['greenpeppers', 'tomatoes'] }
   end
 
   it "should populate mixed fields" do
-    populate_and_check 'http://www.htmlcodetutorial.com/forms/_SELECT_TABINDEX.html', 1, \
-      { 'realname' => 'John Smith', 'email' => 'smith@example.com', 'dep' => 'dev', }
+    populate_and_check 'http://www.htmlcodetutorial.com/forms/_SELECT_TABINDEX.html', { 'realname' => 'John Smith', 'email' => 'smith@example.com', 'dep' => 'dev', }
   end
 
-  def populate_and_check url, form_no, data
+  def populate_and_check url, data, form_no = 0
     @browser.navigate.to url
-    form = @browser.find_elements(:tag_name => 'form')[form_no]
+    form = @browser.find_element(:id => 'content').find_elements(:tag_name => 'form')[form_no]
     form.populate(data).submit
     expect_result data
   end
